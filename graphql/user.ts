64b8f10124c31aa6f9346a24bc45a41fa9fb2669
Lukáš Daněk <dr.danek@gmail.com>
2022-11-24 14:01:14 +0100

    bugfix

--- graphql/user.ts
+++ graphql/user.ts
@@ -245,8 +245,8 @@ export const userGraphql = (dbTable: UZIVATEL): GraphQLSchemaModule => {
               return { error: '', data: '', message: 'Uživatel byl přidán do skupiny.' }
 
             const transporter = nodemailer.createTransport({
-              host: 'smtp10.onebit.cz',
-              port: 587,
+              host: process.env.EMAIL_HOST,
+              port: process.env.EMAIL_PORT,
               auth: {
                 user: process.env.EMAIL_USER,
                 pass: process.env.EMAIL_PASSWORD,
